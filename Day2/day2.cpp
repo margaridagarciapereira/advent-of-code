1805b66ea7d83f790550c96f3a3b21d4e75a9c8a
Margarida Pereira <guidita3@gmail.com>
2017-12-11 22:11:10 +0000

    Add the Part 2 of Day 2

--- Day2/day2.cpp
+++ Day2/day2.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 
@@ -25,6 +26,21 @@ int DiffMaxAndMin(vector<int> row) {
 	return max - min;
 }
 
+int EvenDivisionResult(vector<int> row) {
+	for(int i = 0; i < row.size() - 1; i++) {
+		for(int j = i + 1; j < row.size(); j++) {
+			int rem1 = remainder(row[i], row[j]);
+			if(rem1 == 0)
+				return row[i]/row[j];
+
+			int rem2 = remainder(row[j], row[i]); 
+			if(rem2 == 0)
+				return row[j]/row[i];
+		}
+	}
+	return 0;
+}
+
 int Part1() {
 	int checkSum = 0;
 
@@ -38,8 +54,21 @@ int Part1() {
 	return checkSum;
 }
 
+int Part2() {
+	int checkSum = 0;
+
+	ifstream file("day2.txt");
+	string temp;
+
+	while(getline(file, temp)) {
+		vector<int> rowNumbers = ParseLine(temp);
+		checkSum += EvenDivisionResult(rowNumbers);
+	}
+	return checkSum;
+}
+
 int main() {
 	cout << "Part1: " << Part1() << endl;
-
+	cout << "Part2: " << Part2() << endl;
 	return 0;
 }
