cd5d69beff35655f22f3641c2957e06257f40786
Margarida Pereira <guidita3@gmail.com>
2019-12-02 01:04:21 +0000

    Refactor and add solution for Part 2

--- 2019/Day1/day1.ts
+++ 2019/Day1/day1.ts
@@ -3,9 +3,31 @@ Specifically, to find the fuel required for a module, take its mass,
 divide by three, round down, and subtract 2. */
 import * as fs from "fs";
 
-let sumOfFuel: number = fs
-  .readFileSync("day1_masses.txt", "utf8")
-  .split(/\r?\n/)
-  .reduce((prev, curr) => prev + Math.floor(+curr / 3 - 2), 0);
+const getInitialFuelMasses = () =>
+  fs.readFileSync("day1_masses.txt", "utf8").split(/\r?\n/);
 
-console.log(sumOfFuel);
+const calculateFuelMassNeeded = (mass: number) => Math.floor(mass / 3 - 2);
+
+const getFueldMasses = () =>
+  getInitialFuelMasses().map(mass => calculateFuelMassNeeded(+mass));
+
+const sumOfFuelMasses = getFueldMasses().reduce((prev, curr) => prev + curr);
+
+console.log(`Part 1: ${sumOfFuelMasses}`);
+
+const getFuelNeededByFuel = (fuelMass: number) => {
+  let mass = fuelMass;
+  let totalFuelMass = 0;
+  while (mass >= 0) {
+    mass = calculateFuelMassNeeded(mass);
+    totalFuelMass += mass > 0 ? mass : 0;
+  }
+  return totalFuelMass;
+};
+
+const fuelNeeded = getInitialFuelMasses().reduce(
+  (prev, curr) => prev + getFuelNeededByFuel(+curr),
+  0
+);
+
+console.log(`Part 2: ${fuelNeeded}`);
