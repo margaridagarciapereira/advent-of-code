f193dc261ef692b1b5769cdb18437d32e714408e
Margarida Pereira <guidita3@gmail.com>
2019-12-08 21:01:16 +0000

    Add parameter modes. Part 1 of Day 5 now working

--- 2019/Day5/day5.ts
+++ 2019/Day5/day5.ts
@@ -9,58 +9,136 @@ const loadMemory = () =>
 
 type Operation = "Sum" | "Multiplication" | "Input" | "Output";
 
+type Mode = "Immediate" | "Position";
+
 const executeCalc = (
   instructions: number[],
   operation: Operation,
-  instructionPointer: number,
+  config: Config,
   input?: number
 ) => {
-  const firstParam = instructions[instructionPointer + 1];
-
   if (operation === "Sum") {
-    const secondParam = instructions[instructionPointer + 2];
-    const thirdParam = instructions[instructionPointer + 3];
+    const first =
+      config.firstParam.mode === "Position"
+        ? instructions[config.firstParam.value]
+        : config.firstParam.value;
 
-    instructions[thirdParam] =
-      instructions[firstParam] + instructions[secondParam];
+    const second =
+      config.secondParam.mode === "Position"
+        ? instructions[config.secondParam.value]
+        : config.secondParam.value;
+
+    instructions[config.thirdParam.value] = first + second;
   } else if (operation === "Multiplication") {
-    const secondParam = instructions[instructionPointer + 2];
-    const thirdParam = instructions[instructionPointer + 3];
+    const first =
+      config.firstParam.mode === "Position"
+        ? instructions[config.firstParam.value]
+        : config.firstParam.value;
+    const second =
+      config.secondParam.mode === "Position"
+        ? instructions[config.secondParam.value]
+        : config.secondParam.value;
 
-    instructions[thirdParam] =
-      instructions[firstParam] * instructions[secondParam];
+    instructions[config.thirdParam.value] = first * second;
   } else if (operation === "Output") {
-    console.log(instructions[firstParam]);
+    const first =
+      config.firstParam.mode === "Position"
+        ? instructions[config.firstParam.value]
+        : config.firstParam.value;
+    console.log(first);
   } else if (operation === "Input") {
-    instructions[firstParam] = input;
-  }
+    instructions[config.firstParam.value] = input;
+  } else console.log("Not a valid operation.");
+};
+
+interface Param {
+  value: number;
+  mode: Mode;
+}
+
+interface Config {
+  opCode: number;
+  firstParam?: Param;
+  secondParam?: Param;
+  thirdParam?: Param;
+}
+
+const parseInstruction = (
+  instructions: number[],
+  instructionPointer: number
+): Config | null => {
+  const configCode = instructions[instructionPointer];
+
+  const opCode = configCode % 100;
+
+  const modes = configCode.toString().split("");
+  modes.reverse();
+
+  const firstParamMode: Mode =
+    modes.length < 3 || modes[2] === "0" /*|| opCode === 3 */
+      ? "Position"
+      : "Immediate";
+  const secondParamMode: Mode =
+    modes.length < 4 || modes[3] === "0" ? "Position" : "Immediate";
+  const thirdParamMode: Mode =
+    modes.length < 5 || modes[4] === "0" /*|| opCode === 1 || opCode === 2*/
+      ? "Position"
+      : "Immediate";
+
+  if (opCode === 1 || opCode === 2) {
+    return {
+      opCode: opCode,
+      firstParam: {
+        value: instructions[instructionPointer + 1],
+        mode: firstParamMode
+      },
+      secondParam: {
+        value: instructions[instructionPointer + 2],
+        mode: secondParamMode
+      },
+      thirdParam: {
+        value: instructions[instructionPointer + 3],
+        mode: thirdParamMode
+      }
+    } as Config;
+  } else if (opCode === 3 || opCode === 4) {
+    return {
+      opCode: opCode,
+      firstParam: {
+        value: instructions[instructionPointer + 1],
+        mode: firstParamMode
+      }
+    } as Config;
+  } else if (opCode === 99) {
+    return { opCode: opCode } as Config;
+  } else return null;
 };
 
 const executeProgram = (input: number) => {
   let instructions = loadMemory();
   let instructionPointer = 0;
   let opCode = 0;
-
-  while (opCode !== 99 && instructionPointer < instructions.length - 3) {
-    opCode = instructions[instructionPointer];
+  while (opCode !== 99 && instructionPointer < instructions.length) {
+    const config = parseInstruction(instructions, instructionPointer);
+    opCode = config.opCode;
 
     if (opCode === 1) {
-      executeCalc(instructions, "Sum", instructionPointer);
+      executeCalc(instructions, "Sum", config);
       instructionPointer += 4;
     }
 
     if (opCode === 2) {
-      executeCalc(instructions, "Multiplication", instructionPointer);
+      executeCalc(instructions, "Multiplication", config);
       instructionPointer += 4;
     }
 
     if (opCode === 3) {
-      executeCalc(instructions, "Input", instructionPointer, input);
+      executeCalc(instructions, "Input", config, input);
       instructionPointer += 2;
     }
 
     if (opCode === 4) {
-      executeCalc(instructions, "Output", instructionPointer);
+      executeCalc(instructions, "Output", config);
       instructionPointer += 2;
     }
   }
